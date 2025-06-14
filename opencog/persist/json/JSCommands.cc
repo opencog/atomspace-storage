51ba543d72f2cce0dcfdf166be802dc458e79dab
Linas Vepštas <linasvepstas@gmail.com>
2025-06-13 21:30:14 -0500

    Do a run-time check, not a compile-time mode

--- opencog/persist/json/JSCommands.cc
+++ opencog/persist/json/JSCommands.cc
@@ -38,32 +38,20 @@
 #include "Json.h"
 
 using namespace opencog;
-#define NEW_MCP_STYLE_API
 
 static std::string reterr(const std::string& cmd)
 {
-#ifdef OLD_JS_API
-	return "JSON/JavaScript function not supported: >>" + cmd + "<<\n";
-#endif // OLD_JS_API
-
-#ifdef NEW_MCP_STYLE_API
 	return "{\"success\": false, \"error\": \"Invalid command format\", \"comand\": " + cmd + "\"}";
-#endif // NEW_MCP_STYLE_API
 }
 
 // Common boilerplate
-#ifdef OLD_JS_API
 #define CHK_CMD \
-	pos = cmd.find_first_of("(", epos); \
-	if (std::string::npos == pos) return reterr(cmd); \
-	pos++; \
-	epos = cmd.size();
-#endif // OLD_JS_API
-
-#ifdef NEW_MCP_STYLE_API
-// No-op.
-#define CHK_CMD
-#endif // NEW_MCP_STYLE_API
+	if (js_mode) { \
+		pos = cmd.find_first_of("(", epos); \
+		if (std::string::npos == pos) return reterr(cmd); \
+		pos++; \
+		epos = cmd.size(); \
+	}
 
 #define GET_TYPE \
 	Type t = NOTYPE; \
@@ -147,46 +135,51 @@ std::string JSCommands::interpret_command(AtomSpace* as,
 	if ('#' == cmd[0]) return "";
 	if ('\n' == cmd[0]) return "";
 
-#ifdef NEW_MCP_STYLE_API
-	// Assume that the commands are of the form
-	// { "tool": "someToolName", "params": { stuff }}
-	// e.g. getIncoming as the tool name. Get that name,
-	// move the string pointer to the params argument,
-	// then dispatch on the tool name.
+	// In js-mode, the commands are all of the form
+	//    AtomSpace.someCommand(args)
+	// In MCP-mode, the commands are all of the form
+	//    { "tool": "someToolName", "params": { args }}
+	// So if we find an open brace at the start of the string,
+	// then we are in MCP-mode. Else we are in JS-mode.
 
-	size_t cpos = cmd.find("{ \"tool\": ");
-	if (std::string::npos == cpos) return reterr(cmd);
-	cpos += 10; // 10 == strlen("{ \"tool\": ");
+	bool js_mode = true;
+	if ('{' == cmd[0])
+		js_mode = false;
+	else if ('A' != cmd[0])
+		return reterr(cmd);
 
-	size_t pos = cmd.find_first_not_of("\" \n\t", cpos);
-	if (std::string::npos == pos) return reterr(cmd);
+	size_t cpos, pos, epos;
+	if (js_mode)
+	{
+		cpos = cmd.find_first_of(".");
+		if (std::string::npos == cpos) return reterr(cmd);
 
-	size_t epos = cmd.find_first_of("\", \n\t", pos);
-	if (std::string::npos == epos) return reterr(cmd);
+		cpos = cmd.find_first_not_of(". \n\t", cpos);
+		if (std::string::npos == pos) return reterr(cmd);
 
-	// OK, so now pos and epos bracket the tool name.
-	// Advance cpos past the params.
-	cpos = cmd.find("\"params\": {", epos);
-	if (std::string::npos == cpos) return reterr(cmd);
-	cpos += 13; // 13 == strlen("{ \"params\": {");
-#endif // NEW_MCP_STYLE_API
+		epos = cmd.find_first_of("( \n\t", cpos);
+		if (std::string::npos == epos) return reterr(cmd);
+	}
+	else
+	{
+		cpos = cmd.find("{ \"tool\": ");
+		if (std::string::npos == cpos) return reterr(cmd);
+		cpos += 10; // 10 == strlen("{ \"tool\": ");
 
-#ifdef OLD_JS_API
-	// The older API worked much like above, except that it pretended
-	// that there was a js call happening. So, instead of it being pure
-	// JSON, it looked for AtomSpace.getIncoming(args), hunting for the
-	// dot and the open paren.
-	size_t cpos = cmd.find_first_of(".");
-	if (std::string::npos == cpos) return reterr(cmd);
+		pos = cmd.find_first_not_of("\" \n\t", cpos);
+		if (std::string::npos == pos) return reterr(cmd);
 
-	size_t pos = cmd.find_first_not_of(". \n\t", cpos);
-	if (std::string::npos == pos) return reterr(cmd);
+		epos = cmd.find_first_of("\", \n\t", pos);
+		if (std::string::npos == epos) return reterr(cmd);
 
-	size_t epos = cmd.find_first_of("( \n\t", pos);
-	if (std::string::npos == epos) return reterr(cmd);
-#endif // OLD_JS_API
+		// OK, so now pos and epos bracket the tool name.
+		// Advance cpos past the params.
+		pos = cmd.find("\"params\": {", epos);
+		if (std::string::npos == pos) return reterr(cmd);
+		pos += 13; // 13 == strlen("{ \"params\": {");
+	}
 
-	size_t act = std::hash<std::string>{}(cmd.substr(pos, epos-pos));
+	size_t act = std::hash<std::string>{}(cmd.substr(cpos, epos-cpos));
 
 	// -----------------------------------------------
 	// Get version
